82c8ba0771cf3ecb2327d028908da51353c98751
KenYuChang <justin0915justin0915@gmail.com>
2023-08-27 15:08:01 +0800

    Home screen for products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import Logo from '../assets/Logo.png'
 
 const Header = () => {
     return (
       <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
-            <Navbar.Brand href='/'>E-SHOP</Navbar.Brand>
+            <Navbar.Brand href='/'>
+                <img src={Logo} style={{ width: '40px', height: '50px', marginRight: '5px', borderRadius: '20%' }} alt="E-SHOP" />
+                E-SHOP
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
               <Nav className='ms-auto'>
