b7d90dd73ff4bbcce46460581f694ddecefc8fc4
KenYuChang <justin0915justin0915@gmail.com>
2023-08-27 15:34:33 +0800

    add react-router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,31 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap'
 import Logo from '../assets/Logo.png'
-
 const Header = () => {
     return (
       <header>
         <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
           <Container>
-            <Navbar.Brand href='/'>
+            <LinkContainer to='/'>
+              <Navbar.Brand>
                 <img src={Logo} style={{ width: '40px', height: '50px', marginRight: '5px', borderRadius: '20%' }} alt="E-SHOP" />
                 E-SHOP
-            </Navbar.Brand>
+              </Navbar.Brand>
+            </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
               <Nav className='ms-auto'>
-                <Nav.Link href='/cart'>
-                  <FaShoppingCart /> Cart
-                </Nav.Link>
-                <Nav.Link href='/login'>
-                  <FaUser /> Sign In
-                </Nav.Link>
+                <LinkContainer to='/cart'>
+                  <Nav.Link>
+                    <FaShoppingCart /> Cart
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to='/login'>
+                </LinkContainer>
+                  <Nav.Link>
+                    <FaUser /> Sign In
+                  </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
