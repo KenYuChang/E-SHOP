c5076dcc5cad33b32b51682bad53a1dba6127416
KenYuChang <justin0915justin0915@gmail.com>
2023-08-28 17:10:52 +0800

    logic to controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import asyncHanlder from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import { getProductById, getProducts } from '../controllers/productController.js';
 
-router.get('/', asyncHanlder(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  }));
-router.get('/:id', asyncHanlder(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found');
-    }
-}));
+router.get('/', getProducts);
+router.get('/:id', getProductById);
 
 export default router
