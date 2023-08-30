4cce5f86f75eb3028130ad58454ed159e12e755f
KenYuChang <justin0915justin0915@gmail.com>
2023-08-30 17:42:41 +0800

    Redux store, apiSlice, productsApiSlices, constants

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,30 +1,23 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/rating';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlices';
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState([]);
-
-
   const { id: productId } = useParams();
-
-  useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${productId}`);
-      setProduct(data);
-    }
-    fetchProduct();
-  }, [productId])
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
       
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row>
+
+      { isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+        <Row>
         <Col md={5}>
           <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -76,6 +69,8 @@ const ProductScreen = () => {
           </Card>
         </Col>
       </Row>
+      ) }
+      
     </>
   )
 }
