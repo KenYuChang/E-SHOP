8641612bf5a42691901265dfc50641365700b67b
KenYuChang <justin0915justin0915@gmail.com>
2023-09-01 15:56:29 +0800

    user logout

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -12,9 +12,13 @@ const authSlice = createSlice({
       state.info = action.payload;
       localStorage.setItem('userInfo', JSON.stringify(action.payload));
     },
+    logout: (state, action) => {
+      state.userInfo = null;
+      localStorage.removeItem('userInfo');
+    },
   },
 });
 
-export const { setCredential } = authSlice.actions;
+export const { setCredential, logout } = authSlice.actions;
 
 export default authSlice.reducer;
