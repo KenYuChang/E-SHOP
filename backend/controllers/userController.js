306147a85c237409ee1dbbc85318d2e3b1bb7f8c
KenYuChang <justin0915justin0915@gmail.com>
2023-09-01 13:12:53 +0800

    register user controller & generateToken utils

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHanlder from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Auth user & get token
 // @route GET /api/users/login
@@ -10,17 +10,7 @@ const authUser = asyncHanlder(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-
-    // set JWT as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -38,13 +28,38 @@ const authUser = asyncHanlder(async (req, res) => {
 // @route POST /api/users
 // @access public
 const registerUser = asyncHanlder(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc Logout user / clear cookie
 // @route POST /api/users/logout
 // @access private
 const logoutUser = asyncHanlder(async (req, res) => {
+  // clear cookie
   res.cookie('jwt', '', {
     httpOnly: true,
     expires: new Date(0),
