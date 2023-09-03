2be6a0d55b5272f41082f804b3f45783e745532b
KenYuChang <justin0915justin0915@gmail.com>
2023-09-03 16:10:35 +0800

    paypal set up and order pay

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -59,7 +59,23 @@ const getOrderById = asyncHanlder(async (req, res) => {
 // @route PUT /api/orders/:id/pay
 // @access private
 const updateOrderToPaid = asyncHanlder(async (req, res) => {
-  res.send('update order to paid');
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('order not found');
+  }
 });
 
 // @desc update order to deliver
