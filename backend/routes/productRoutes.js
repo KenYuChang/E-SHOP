eb78a81ada1c9e33585083742a93cb07554480de
KenYuChang <justin0915justin0915@gmail.com>
2023-09-05 17:43:16 +0800

    create reviews in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/auth.js';
 
@@ -14,4 +15,5 @@ router.post('/', protect, admin, createProduct);
 router.get('/:id', getProductById);
 router.put('/:id', protect, admin, updateProduct);
 router.delete('/:id', protect, admin, deleteProduct);
+router.post('/:id/reviews', protect, createProductReview);
 export default router;
