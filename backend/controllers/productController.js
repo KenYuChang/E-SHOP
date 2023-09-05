c7bd9ef5de051181369a5efa3ce559efb061636b
KenYuChang <justin0915justin0915@gmail.com>
2023-09-05 14:03:46 +0800

    create product by admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,23 +5,41 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access public
 const getProducts = asyncHanlder(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+  const products = await Product.find({});
+  res.json(products);
 });
-
 
 // @desc Fetch a product
 // @route GET /api/products/:id
 // @access public
 const getProductById = asyncHanlder(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Resource not found');
-    }
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Resource not found');
+  }
 });
 
-export {getProducts, getProductById};
+// @desc create a product
+// @route POST /api/products
+// @access private/admin
+const createProduct = asyncHanlder(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductById, createProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { getProductById, getProducts } from '../controllers/productController.js';
+import { getProductById, getProducts, createProduct } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/auth.js';
+import { create } from 'express-handlebars';
 
 router.get('/', getProducts);
+router.post('/', protect, admin, createProduct);
 router.get('/:id', getProductById);
 
-export default router
+export default router;
