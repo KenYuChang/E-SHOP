919d5766942649f70a1db1cb48a30dc5dcc9d1d3
KenYuChang <justin0915justin0915@gmail.com>
2023-09-05 15:09:50 +0800

    update product by admin excluding upload image

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { getProductById, getProducts, createProduct } from '../controllers/productController.js';
+import { getProductById, getProducts, createProduct, updateProduct } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/auth.js';
-import { create } from 'express-handlebars';
 
 router.get('/', getProducts);
 router.post('/', protect, admin, createProduct);
 router.get('/:id', getProductById);
-
+router.put('/:id', protect, admin, updateProduct);
 export default router;
